76a90b22eb5ed4d31db92056aed246c6373a373a
KriK96 <skuf962@yandex.ru>
2018-03-08 21:36:41 +0300

    Добавлено меню, переход к следующему слову + небольшой рефакторинг компонента Form

--- src/store/WordsStore.jsx
+++ src/store/WordsStore.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { RouterStore } from 'mobx-react-router';
 
 class WordStore extends React.Component {
-    @observable
+
 }
 
 export const wordStore = new WordStore();
